5b314efabb4dc38d0c5590bbd180e58864719780
mgharris97 <165623402+mgharris97@users.noreply.github.com>
2025-11-29 22:09:11 +0200

    Established skeleton for FastAPI endpoints and their methods in main.py

--- app/main.py
+++ app/main.py
@@ -7,11 +7,45 @@ app = FastAPI(title="Chili labs Backend Task")
 #make a table on startup
 Base.metadata.create_all(bind=engine)
 
-# GET, PUT, POST, DELETE
+# Endpoints
+# Register a user, Login a user, Upload avatar, Open WebSocket, avatar changing notification, Delete user
+# HTTP methods POST(create), GET (retrieve/read), DELETE(remove), PUT(replace entirely), PATCH(update partially), WS(real time communication)
 
-@app.get('/')
-def health_check():
-    return {"status": "Success", "data": "API is running!"}
+
+# register user
+@app.post('/register')
+def register_user():
+    pass
+
+# create a new token
+@app.post('/login')
+def login_user():
+    pass
+
+# Avatar upload 
+@app.post('/avatar')
+def upload_avatar():
+    pass
+
+# Avatar change notification
+@app.websocket('/ws/avatar')
+def change_avatar():
+    pass
+
+# Delete user
+@app.delete('/user')
+def delete_user():
+    pass
+
+# Health
+@app.get('/health')
+def health():
+    pass
+
+
+
+
+
 
 
 
